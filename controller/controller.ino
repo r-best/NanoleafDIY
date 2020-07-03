e5bcf5b9775a01b65f02da4e23aa6d9dd7ea44fe
r-best <bobbyisbest3@gmail.com>
2020-07-02 22:58:53 -0400

    Adding webserver capability + correcting tree decoding algorithm

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,22 +1,82 @@
 #include <string.h>
 #include <stdlib.h>
 
-#include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
 
 #include "tree.h"
 #include "utils.h"
 #include "constants.h"
 
 
+const char* WIFI_SSID = "***REMOVED***";
+const char* WIFI_PASS = "***REMOVED***";
+
+ESP8266WebServer server(80);
+
 void setup() {
     Serial.begin(115200);
     while(!Serial){;}
+    Serial.println("Starting up");
 
-    Serial.println("Ready!");
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+    Serial.print("Connecting");
+    while (WiFi.status() != WL_CONNECTED){
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println();
+
+    Serial.print("Connected, IP address: ");
+    Serial.println(WiFi.localIP());
+
+    if(!MDNS.begin("lightpanels")){
+        Serial.println("ERROR");
+    }
+
+    server.on("/", [](){ server.send(200, "text/plain", "OK"); });
+    server.on("/network", [](){
+        char* response = build_response(tree_encoding);
+        server.send(200, "application/json", response);
+        free(response);
+    });
+
+    server.begin();
+    MDNS.addService("http", "tcp", 80);
 
     // Wait one second to make sure all connected panels come online
     delay(1000);
 
+    // Discover all connected panels
+    discover_network_DEBUG();
+}
+
+/**
+ * Main control loop; 
+ */
+void loop() {
+    MDNS.update();
+    server.handleClient();
+}
+
+char* build_response(char* data){
+    char* response = (char*)malloc(strlen(data) + 15);
+    strcpy(response, "{ \"data\": \"");
+    strcat(response, data);
+    strcat(response, "\" }");
+    return response;
+}
+
+void discover_network_DEBUG(){
+    const char* input = "(((XX)X)(X((XX)X)))";
+    char* tree = (char*)malloc(strlen(input));
+    strcpy(tree, input);
+    parse_tree(tree);
+}
+
+void discover_network(){
     // Send discovery command to connected panel in
     // a loop until it responds "Acknowledged!"
     while(true){
@@ -35,12 +95,5 @@ void setup() {
 
     // After discovery acknowledgement, wait for response with full network tree
     char *tree = readSerial(DISCOVERY_RESPONSE_TIMEOUT);
-    Node* root = parse_tree(tree);
-}
-
-/**
- * Main control loop; 
- */
-void loop() {
-    
+    parse_tree(tree);
 }
--- controller/tree.h
+++ controller/tree.h
@@ -2,21 +2,43 @@
 #define TREE_H
 
 
-typedef struct {
+typedef struct Node {
     Node* parent;
     Node* left;
     Node* right;
     char* directions;
 } Node;
 
+char* tree_encoding = NULL;
+Node* root = NULL;
+
+/**
+ * Recursive funtion to free up all the malloc'd memory
+ * used by the network tree, so the tree can be recalculated
+ * Should be called with the root node as the argument
+ */
+void clear_tree(Node* node){
+    free(node->directions);
+    if(node->left) clear_tree(node->left);
+    if(node->right) clear_tree(node->right);
+    free(node);
+}
+
 /**
  * Algorithm to parse the network structure from the given string
  */
-Node* parse_tree(char* tree){
-    int input_length = strlen(tree);
+void parse_tree(char* tree){
+    tree_encoding = tree;
+    int input_length = strlen(tree_encoding);
 
-    Node* root = (Node*)malloc(sizeof(Node));
-    root->directions = "";
+    if(root != NULL){
+        clear_tree(root);
+        free(tree_encoding);
+    }
+
+    root = (Node*)malloc(sizeof(Node));
+    root->directions = (char*)malloc(1);
+    strcpy(root->directions, "");
     root->parent = NULL;
     root->left = NULL;
     root->right = NULL;
@@ -26,10 +48,10 @@ Node* parse_tree(char* tree){
     int active_directions_length = 1;
     for(int i = 1; i < input_length-1; i++){
         // On an open parenthesis, we add a new node to the tree
-        if(tree[i] == '('){
+        if(tree_encoding[i] == '('){
             Node *child = (Node*)malloc(sizeof(Node));
             child->parent = active;
-            child->directions = (char*)malloc(active_directions_length+1);
+            child->directions = (char*)malloc(active_directions_length++);
             strcpy(child->directions, active->directions);
             if(next_is_right){
                 active->right = child;
@@ -44,13 +66,11 @@ Node* parse_tree(char* tree){
         }
         // On a close parenthesis, we go back to the parent node and set
         // that the next node added will be on the right
-        else if (tree[i] == ')'){
+        else if (tree_encoding[i] == ')'){
             active = active->parent;
             next_is_right = true;
         }
     }
-
-    return root;
 }
 
 #endif
